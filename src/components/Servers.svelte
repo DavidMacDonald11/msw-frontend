31461772dc26a528307ca2591abe48fd16aa6846
david <davidjmacdonald11@gmail.com>
2022-09-06 12:48:30 -0400

    Move server state into own component

--- src/components/Servers.svelte
+++ src/components/Servers.svelte
@@ -1,22 +1,6 @@
 <script>
-  import api from "../lib/api"
-  import {servers, state, status, updater} from "../stores"
-
-  const getIp = server => {
-    const port = server.public.port
-    return port == 25565 ? $state.ip : `${$state.ip}:${port}`
-  }
-
-  const startServer = async server => {
-    try {
-      server.state.loading = true
-
-      await api("post", "/api/query/start", {id: server.public.id})
-      await updater.run()
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  import {servers, status} from "../stores"
+  import State from "./server/State.svelte"
 </script>
 
 <div class="flex-container horizontal {$status.isOn ? '' : 'not-visible'}">
@@ -27,26 +11,7 @@
         <p>{server.public.description}</p>
       </div>
       <hr class="flex-item" />
-      {#if server.state.loading}
-        <h3>Loading...</h3>
-      {:else if server.state.isOn}
-        <h3>{server.public.edition} Edition - {server.public.version}</h3>
-        <h3>IP - {getIp(server)}</h3>
-        {#if server.state.playerCount > 0}
-          <h3>{server.state.playerCount} online</h3>
-        {:else}
-          <h3>0 online - {server.state.minutesLeft.toFixed(1)} Mins Left</h3>
-        {/if}
-      {:else}
-        <div class="flex-item">
-          <button
-            disabled={!$status.isOn}
-            on:click={() => {
-              startServer(server)
-            }}>Start Server</button
-          >
-        </div>
-      {/if}
+      <State {server} />
     </div>
   {/each}
 </div>
@@ -57,14 +22,6 @@
     border: 1px solid;
     border-radius: 8px;
     margin: 20px;
-  }
-
-  .flex-item.server button {
-    margin: 10px 0;
-  }
-
-  .flex-item.server h3 {
-    margin: 7px 0;
   }
 
   .not-visible > * {
---
+++ src/components/server/State.svelte
@@ -0,0 +1,54 @@
+<script>
+  import api from "../../lib/api"
+  import {state, status, updater} from "../../stores"
+
+  export let server
+
+  const getIp = server => {
+    const port = server.public.port
+    return port == 25565 ? $state.ip : `${$state.ip}:${port}`
+  }
+
+  const startServer = async server => {
+    try {
+      await api("post", "/api/query/start", {id: server.public.id})
+      await updater.run()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+</script>
+
+<div class="flex-item">
+  {#if server.state.loading}
+    <h3>Loading...</h3>
+  {:else if server.state.isOn}
+    <h3>{server.public.edition} Edition - {server.public.version}</h3>
+    <h3>IP - {getIp(server)}</h3>
+    {#if server.state.playerCount > 0}
+      <h3>{server.state.playerCount} online</h3>
+    {:else}
+      <h3>0 online - {server.state.minutesLeft.toFixed(1)} Mins Left</h3>
+    {/if}
+  {:else}
+    <div class="flex-item">
+      <button
+        disabled={!$status.isOn}
+        on:click={event => {
+          event.currentTarget.disabled = true
+          startServer(server)
+        }}>Start Server</button
+      >
+    </div>
+  {/if}
+</div>
+
+<style>
+  button {
+    margin: 10px 0;
+  }
+
+  h3 {
+    margin: 7px 0;
+  }
+</style>
